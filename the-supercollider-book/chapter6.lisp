f1737a107660aa32ed08356e3eabb79a371e1993
Nuno Trocado <ntrocado@gmail.com>
2019-04-19 02:04:43 +0100

    Cleanup and examples up to page 190

--- the-supercollider-book/chapter6.lisp
+++ the-supercollider-book/chapter6.lisp
@@ -1,8 +1,12 @@
-(ql:quickload '("cl-patterns"
-		"cl-patterns/supercollider"))
+;;;; Supercollider Book - Lisp version of the code
+;;;; Chapter 6
 
-(defparameter *clock* (make-clock (/ 110 60)))
+;;; Cl-patterns is a library providing the equivalent functionality of patterns in SuperCollider
+(ql:quickload "cl-patterns/supercollider")
 
+(in-package :cl-patterns)
+
+(defparameter *clock* (make-clock 110/60))
 
 ;;; Figure 6.1
 
@@ -14,9 +18,10 @@
 		(amp 0.4)
 		(pan 0)
 		(ar 1)
-		(dr 1))
-  (let* ((audio (sin-osc.ar freq 0 amp))
-	 (audio (* audio (linen.kr gate ar 1 dr :act :free)))
+		(dr 1)
+		(detune 0))
+  (let* ((audio (sin-osc.ar (+ freq detune) 0 amp))
+	 (audio (* audio (env-gen.kr (asr ar 1 dr) :gate gate :act :free)))
 	 (audio (pan2.ar audio pan)))
     (offset-out.ar out audio)))
 
@@ -38,23 +43,60 @@
 
 ;;; Figure 6.4
 
-(defparameter *a*
-  (pbind :dur 0.2
-	 :freq (pseq '(100 200 300 400 500 600 700 800) 1)))
+(pbind :dur 0.2
+       :freq (pseq '(100 200 300 400 500 600 700 800) 1))
 
 ;;; Figure 6.5
 
-(defparameter *p*
-  (pbind :instrument :sine
-	 :detune '(0 1 3)
-	 :freq (pseq (seq-range 100 1200 100)
-		     (* 4 5 7))
-	 :db (pseq '(-20 -40 -30 -40) :inf)
-	 :pan (pseq '(-1 0 1 0) :inf)
-	 :dur (pseq '(0.2 0.2 0.2 0.2 0.4 0.4 0.8) :inf)
-	 :legato (pseq '(2 0.5 0.75 0.5 0.25) :inf)))
+(pb :p
+  :instrument :sine
+  :detune '(0 1 3)
+  :freq (pseq (seq-range 100 1200 100)
+	      (* 4 5 7))
+  :db (pseq '(-20 -40 -30 -40) :inf)
+  :pan (pseq '(-1 0 1 0) :inf)
+  :dur (pseq '(0.2 0.2 0.2 0.2 0.4 0.4 0.8) :inf)
+  :legato (pseq '(2 0.5 0.75 0.5 0.25) :inf))
 
-(play *p*)
+(play :p)
+(stop :p)
 
 ;;; Figure 6.6
 ;;; 'Group' event keyword is not implemented yet.
+
+;;; Figure 6.7
+;; 2nd inversion - e loudest
+(play (event :instrument :sine
+	     :degree '(-3 0 2)
+	     :sustain 2
+	     :db '(-20 -20 -10)))
+;; 2nd inversion - c loudest
+(play (event :instrument :sine
+	     :degree '(-3 0 2)
+	     :sustain 2
+	     :db '(-20 -10 -20)))
+;; note "fattened"
+(play (event :instrument :sine
+	     :degree 0
+	     :sustain 2
+	     :detune '(0 3 5)))
+;; each detune is assigned to a different pitch, fat free
+(play (event :instrument :sine
+	     :degree '(-3 2 4)
+	     :sustain 2
+	     :detune '(0 3 5))
+;; detune rotates through each note in the chord
+(play (event :instrument :sine
+	     :degree '(-3 2 4)
+	     :sustain 2
+	     :detune '(0 0 0 3 3 3 5 5 5))))
+
+;;; 6.3.2 Combining Patterns
+
+(defparameter *a* (pbind :instrument :sine
+			 :dur (pseq '(0.4) 5)))
+(defparameter *b* (pbind :instrument :sine
+			 :degree (pseq '(10 6))
+			 :dur (pseq '(0.5) 4)))
+(play (pseq (list *a* *b*) 2))
+(play (ppar (list *a* *b*)))
