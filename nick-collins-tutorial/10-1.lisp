90c8cc3474f444e3036b9f56297045d8c9fe45ee
Nuno Trocado <ntrocado@gmail.com>
2019-07-26 23:32:05 +0100

    Finish chapter 10.1

--- nick-collins-tutorial/10-1.lisp
+++ nick-collins-tutorial/10-1.lisp
@@ -8,3 +8,20 @@
   (send-message *s* "/s_new" "default" id 0 1)
   (sc::sched-add (sc::scheduler *s*) (+ (now) 1) #'send-message *s* "/n_free" id))
 
+;;; Receiving OSC messages from the server
+;;; The following is the functionality currently offered by cl-collider:
+
+;;; register to receive a trigger message
+(flet ((osc-responder (node id value)
+	 (format t "~&Received trigger! ~a ~a ~a~%" node id value)))
+  (add-reply-responder "/tr" #'osc-responder))
+
+(play (send-trig.ar (impulse.ar 0.5)))
+
+;;; keep it running and now run these lines:
+(flet ((osc-responder (node id value)
+	 (format t "~&New reaction! ~a ~a ~a~%" node id value)))
+  (add-reply-responder "/tr" #'osc-responder))
+
+;;; remove the reply responder
+(remove-reply-responder "/tr")
